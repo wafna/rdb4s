d418983103f79888231824dcb9fa0a13b7b3ec0c
Jason Feingold <jfeingold@gmail.com>
2018-10-31 10:09:43 -0800

    Added settings for connetion testing.

--- src/main/scala/wafna/rdb4s/db/ConnectionPool.scala
+++ src/main/scala/wafna/rdb4s/db/ConnectionPool.scala
@@ -1,9 +1,7 @@
 package wafna.rdb4s.db
 import java.util.concurrent.atomic.AtomicBoolean
-
 import wafna.rdb4s.bracket
 import wafna.rdb4s.db.RDB.{Connection, ConnectionManager, DBPromise}
-
 import scala.annotation.tailrec
 import scala.concurrent.duration._
 import scala.util.Try
@@ -19,36 +17,76 @@ trait ConnectionPool[+R <: Connection] {
   * A dynamically sized thread pool where each thread is assigned one connection.
   */
 object ConnectionPool {
+  /**
+    * Configuration for a ConnectionPool.
+    * Non-reentrant.
+    */
   class Config() {
     private var _name: Option[String] = None
     private var _maxPoolSize: Option[Int] = None
     private var _minPoolSize: Option[Int] = None
     private var _idleTimeout: Option[FiniteDuration] = None
+    private var _connectionTestCycleLength: Option[FiniteDuration] = None
+    private var _connectionTestTimeout: Option[Int] = None
     private var _maxQueueSize: Option[Int] = None
     def name: Option[String] = _name
+    /**
+      * Prepended to the thread names in the connection pool for easy identification.
+      */
     def name(n: String): this.type = {
       _name = Some(n)
       this
     }
     def maxPoolSize: Option[Int] = _maxPoolSize
+    /**
+      * The maximum number of tasks allowed to queue waiting for execution.
+      */
     def maxPoolSize(i: Int): this.type = {
       if (1 > i) throw new IllegalArgumentException(s"maxPoolSize must be positive, got $i")
       _maxPoolSize = Some(i)
       this
     }
     def minPoolSize: Option[Int] = _minPoolSize
+    /**
+      * The minimum number of connections to maintain.
+      */
     def minPoolSize(i: Int): this.type = {
       if (0 > i) throw new IllegalArgumentException(s"minPoolSize must be non-negative, got $i")
       _minPoolSize = Some(i)
       this
     }
     def idleTimeout: Option[FiniteDuration] = _idleTimeout
+    /**
+      * The amount of time a thread's connection is not used before the connection is discarded (along with the thread, of course).
+      */
     def idleTimeout(t: FiniteDuration): this.type = {
       if (1 > t.toMillis) throw new IllegalArgumentException(s"idleTimeout must be positive, got $t")
       _idleTimeout = Some(t)
       this
     }
+    def connectionTestCycleLength: Option[FiniteDuration] = _connectionTestCycleLength
+    /**
+      * The interval of time between tests of the connection for viability.  This is implemented with Connection.isValid.
+      * Note that if it's longer than idleTimeout it will never get tested unless there is a non-zero minPoolSize.
+      */
+    def connectionTestCycleLength(t: FiniteDuration): this.type = {
+      if (0 > t.toMillis) throw new IllegalArgumentException(s"connectionTestCycleLengthTimeout must be positive, got $t")
+      _connectionTestCycleLength = Some(t)
+      this
+    }
+    def connectionTestTimeout: Option[Int] = _connectionTestTimeout
+    /**
+      * The length of time (in seconds, per Connection.isValid) to spend waiting for tasks to wait to test the connection for viability.
+      */
+    def connectionTestTimeout(t: Int): this.type = {
+      if (0 > t) throw new IllegalArgumentException(s"connectionTestTimeout must be non-negative, got $t")
+      _connectionTestTimeout = Some(t)
+      this
+    }
     def maxQueueSize: Option[Int] = _maxQueueSize
+    /**
+      * The maximum number of tasks to buffer in a FIFO queue.
+      */
     def maxQueueSize(i: Int): this.type = {
       if (1 > i) throw new IllegalArgumentException(s"maxQueueSize must be positive, got $i")
       _maxQueueSize = Some(i)
@@ -65,6 +103,8 @@ object ConnectionPool {
     val minPoolSize = config.minPoolSize getOrElse 0
     if (minPoolSize > maxPoolSize) sys error s"minPoolSize ($minPoolSize) > maxPoolSize ($maxPoolSize)"
     val idleTimeout = config.idleTimeout getOrElse sys.error("idleTimeout required.")
+    val connectionTestCycleLength = config.connectionTestCycleLength getOrElse sys.error("connectionTestCycleLength required.")
+    val connectionTestTimeout = config.connectionTestTimeout getOrElse sys.error("connectionTestTimeout required.")
     val maxQueueSize = config.maxQueueSize getOrElse sys.error("maxQueueSize required.")
     if (1 > maxQueueSize) sys error s"maxQueueSize must be positive"
     val active = new AtomicBoolean(true)
@@ -107,17 +147,33 @@ object ConnectionPool {
     }
     class TaskRunner[T](val runnerName: String) extends Runnable {
       override def run(): Unit = {
+        val start = System.currentTimeMillis()
+        var lastTask = System.currentTimeMillis()
         listener threadStart threadPool.size
         try {
           bracket(connectionManager.createConnection())(_.close()) { connection =>
             @tailrec def runTasks(): Unit =
               Option(
-                queue.poll(idleTimeout.toMillis, TimeUnit.MILLISECONDS)
+                // We awaken from waiting on the queue to test the connection.
+                // This strategy allows us to reserve testing connections for when there is no other activity.
+                queue.poll(connectionTestCycleLength.toMillis, TimeUnit.MILLISECONDS)
               ) match {
                 case None =>
-                  // nothing on the queue: time to bail
-                  Unit
+                  val now = System.currentTimeMillis()
+                  if (!connection.isValid(connectionTestTimeout)) {
+                    // The simplest thing to do here is simply drop the whole thread and start a new one, with a new
+                    // connection, of course, as needed, below.
+                    listener.connectionDropped((now - start).milli)
+                  } else if (synch.synchronized(threadPool.size) > minPoolSize && (now - lastTask) >= idleTimeout.toMillis) {
+                    // Just pruning superfluous connections.
+                    listener.threadIdleTimeout((now - start).milli)
+                  } else {
+                    runTasks()
+                  }
                 case Some(task) =>
+                  // We consume the connection uncritically here because we know it's been used or tested no more than
+                  // connectionTestCycleLength milliseconds ago.
+                  lastTask = System.currentTimeMillis()
                   listener.taskStart(queue.size(), (System.currentTimeMillis() - task.created).millis)
                   val t0 = System.currentTimeMillis()
                   try
