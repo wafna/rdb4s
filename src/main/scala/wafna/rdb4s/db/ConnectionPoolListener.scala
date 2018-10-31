3172e759ba3e83e61d4b9e42d1828500a4729a7d
Jason Feingold <jfeingold@gmail.com>
2018-10-31 10:10:15 -0800

    Added connectionDropped and threadIdleTimeout events.

--- src/main/scala/wafna/rdb4s/db/ConnectionPoolListener.scala
+++ src/main/scala/wafna/rdb4s/db/ConnectionPoolListener.scala
@@ -1,5 +1,5 @@
 package wafna.rdb4s.db
-import scala.concurrent.duration.Duration
+import scala.concurrent.duration.FiniteDuration
 /**
   * Listens to events in the connection pool.  Used for gathering metrics.
   * These methods are called in line rather than being driven off a message bus
@@ -8,10 +8,12 @@ import scala.concurrent.duration.Duration
 class ConnectionPoolListener {
   def poolStart(): Unit = Unit
   def poolStop(queueSize: Int): Unit = Unit
-  def taskStart(queueSize: Int, timeInQueue: Duration): Unit = Unit
-  def taskStop(queueSize: Int, timeToExecute: Duration): Unit = Unit
+  def taskStart(queueSize: Int, timeInQueue: FiniteDuration): Unit = Unit
+  def taskStop(queueSize: Int, timeToExecute: FiniteDuration): Unit = Unit
   def threadStart(threadPoolSize: Int): Unit = Unit
   def threadStop(threadPoolSize: Int): Unit = Unit
+  def connectionDropped(duration: FiniteDuration): Unit = Unit
+  def threadIdleTimeout(duration: FiniteDuration): Unit = Unit
 }
 /**
   * Static instance for implicit defaults.
